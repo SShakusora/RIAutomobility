9c44ffa1ae001ff3a7f9b5e2ed7508c35c2a5bb9
SShakusora <1404928002@qq.com>
2025-12-08 21:14:39 +0800

    fix: fix assemble item shrink.

--- src/main/java/com/sshakusora/riautomobility/mixin/AutoMechanicTableRecipeMixin.java
+++ src/main/java/com/sshakusora/riautomobility/mixin/AutoMechanicTableRecipeMixin.java
@@ -2,14 +2,17 @@ package com.sshakusora.riautomobility.mixin;
 
 import io.github.foundationgames.automobility.recipe.AutoMechanicTableRecipe;
 import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -19,20 +22,28 @@ import java.util.function.Consumer;
 public class AutoMechanicTableRecipeMixin {
     @Final
     @Shadow protected Set<Ingredient> ingredients;
+    @Final
+    @Shadow protected ItemStack result;
 
-    @Inject(method = "forMissingIngredients", at = @At("HEAD"), remap = false, cancellable = true)
-    private void forMissingIngredientsHead(Container inv, Consumer<Ingredient> action, CallbackInfo ci) {
-        boolean isIngredientHasTag = false;
+    @Inject(method = "assemble(Lnet/minecraft/world/SimpleContainer;)Lnet/minecraft/world/item/ItemStack;", at = @At("HEAD"), remap = false, cancellable = true)
+    private void assembleFix(SimpleContainer inv, CallbackInfoReturnable<ItemStack> cir) {
+        if(!ingredientHasTag()) return;
+
         for(Ingredient ingredient : this.ingredients) {
-            for(ItemStack is : ingredient.getItems()) {
-                if(is.hasTag()){
-                    isIngredientHasTag = true;
+            for(int i = 0; i < inv.getContainerSize(); ++i) {
+                ItemStack stack = inv.getItem(i);
+                if (ItemStack.isSameItemSameTags(stack, ingredient.getItems()[0])) {
+                    stack.shrink(1);
                     break;
                 }
             }
-            if(isIngredientHasTag) break;
         }
-        if(!isIngredientHasTag) return;
+        cir.setReturnValue(this.result.copy());
+    }
+
+    @Inject(method = "forMissingIngredients", at = @At("HEAD"), remap = false, cancellable = true)
+    private void forMissingIngredientsHead(Container inv, Consumer<Ingredient> action, CallbackInfo ci) {
+        if(!ingredientHasTag()) return;
 
         ArrayList<ItemStack> invCopy = new ArrayList<>();
         for(int i = 0; i < inv.getContainerSize(); ++i) {
@@ -56,5 +67,17 @@ public class AutoMechanicTableRecipeMixin {
             }
         }
         ci.cancel();
+    }
+
+    @Unique
+    private boolean ingredientHasTag(){
+        for(Ingredient ingredient : this.ingredients) {
+            for(ItemStack is : ingredient.getItems()) {
+                if(is.hasTag()){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
