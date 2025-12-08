2efcb55b16686ca0eb10a5230268275ef1dee5f8
SShakusora <1404928002@qq.com>
2025-12-08 18:28:08 +0800

    fix: fix recipe

--- src/main/java/com/sshakusora/riautomobility/mixin/AutoMechanicTableRecipeMixin.java
+++ src/main/java/com/sshakusora/riautomobility/mixin/AutoMechanicTableRecipeMixin.java
@@ -1,32 +1,60 @@
 package com.sshakusora.riautomobility.mixin;
 
 import io.github.foundationgames.automobility.recipe.AutoMechanicTableRecipe;
+import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.function.Consumer;
 
-//TODO: fix extend recipes
 @Mixin(AutoMechanicTableRecipe.class)
 public class AutoMechanicTableRecipeMixin {
-//    @Redirect(
-//            method = "forMissingIngredients",
-//            at = @At(
-//                    value = "INVOKE",
-//                    target = "Ljava/util/stream/Stream;noneMatch(Ljava/util/function/Predicate;)Z"
-//            ),
-//            remap = false
-//    )
-//    private boolean redirectNoneMatchToAllMatch(Stream<ItemStack> stream, Predicate<ItemStack> predicate) {
-//        return stream.anyMatch(stack -> {
-//            Ingredient ingredient = (Ingredient) predicate;
-//            System.out.println("Ingredient: " + Arrays.toString(ingredient.getItems()) + "stack: " + stack);
-//            return !ItemStack.isSameItemSameTags(stack, ingredient.getItems()[0]);
-//        });
-//    }
+    @Final
+    @Shadow protected Set<Ingredient> ingredients;
+
+    @Inject(method = "forMissingIngredients", at = @At("HEAD"), remap = false, cancellable = true)
+    private void forMissingIngredientsHead(Container inv, Consumer<Ingredient> action, CallbackInfo ci) {
+        boolean isIngredientHasTag = false;
+        for(Ingredient ingredient : this.ingredients) {
+            for(ItemStack is : ingredient.getItems()) {
+                if(is.hasTag()){
+                    isIngredientHasTag = true;
+                    break;
+                }
+            }
+            if(isIngredientHasTag) break;
+        }
+        if(!isIngredientHasTag) return;
+
+        ArrayList<ItemStack> invCopy = new ArrayList<>();
+        for(int i = 0; i < inv.getContainerSize(); ++i) {
+            invCopy.add(inv.getItem(i));
+        }
+
+        for(Ingredient ingredient : this.ingredients) {
+            boolean flag = false;
+            int removeIdx = 0;
+            for(int i = 0; i < invCopy.size(); i++) {
+                if(ItemStack.isSameItemSameTags(ingredient.getItems()[0], invCopy.get(i))){
+                    removeIdx = i;
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                action.accept(ingredient);
+            } else {
+                invCopy.remove(removeIdx);
+            }
+        }
+        ci.cancel();
+    }
 }
